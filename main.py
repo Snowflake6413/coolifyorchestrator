7cec58941898140ed6c7217073819aa82a2a0758
Snowflake6413 <verycoolandsmart@protonmail.com>
2026-01-25 23:15:33 -0500

    add: coolify version and health commands

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import requests
 import time
 from dotenv import load_dotenv
 from slack_bolt import App
@@ -8,8 +9,12 @@ load_dotenv()
 
 
 # -------------- Envs -------------
+# SLACK :3
 SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
 SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
+# Coolify
+COOLIFY_API_URL = os.getenv("COOLIFY_API_URL")
+COOLIFY_API_KEY = os.getenv("COOLIFY_API_KEY")
 
 
 app = App(token=SLACK_BOT_TOKEN)
@@ -24,7 +29,32 @@ def ping_pong(ack, respond):
     respond(f"pong! latency: {latency}ms")
 
 
+@app.command("/coolify-version")
+def coolify_ver(ack, respond):
+        ack()
+        response = requests.get(
+            f"{COOLIFY_API_URL}/version",
+            headers={"Authorization" : f"Bearer {COOLIFY_API_KEY}"}
+            )
+        if response.status_code == 200:
+            respond(f"Coolify Ver: {response.text}")
+        else:
+            respond(f"Error! {response.status_code}")
 
+
+
+@app.command("/coolify-health-status")
+def coolify_status(ack, respond):
+     ack()
+     response = requests.get(
+          f"{COOLIFY_API_URL}/version",
+          headers={"Authorization" : f"Bearer {COOLIFY_API_KEY}"}
+        )
+     if response.status_code == 200:
+          respond(f"Coolify return health status: {response.text}")
+     else:
+          respond(f"Error! {response.status_code}")
+ 
 
 
 
